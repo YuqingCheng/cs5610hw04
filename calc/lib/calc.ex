5fb236eedf04032baf50b3d1c50855793740d15c
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-02-02 20:35:53 -0500

    implement function without parenthesis

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -1,18 +1,45 @@
 defmodule Calc do
-  @moduledoc """
-  Documentation for Calc.
-  """
+  def eval(line) do
+    list = String.split(line, " ")
+    vals = %{:toadd => 0.0, :totimes => 1.0}
+    map = %{:vals => vals, :list => list}
+    eval_list(map)
 
-  @doc """
-  Hello world.
+  end
 
-  ## Examples
+  defp eval_list(map) do
+    [h1 | t1] = map.list
 
-      iex> Calc.hello
-      :world
+    if is_open_paren h1 do
+      temp = String.slice(h1, 1, String.length(h1))
+    end
 
-  """
-  def hello do
-    :world
+    num = Float.parse(h1) |> elem(0)
+
+    if length(t1) == 0 do
+      map.vals.totimes * num + map.vals.toadd
+    else
+      [h2 | t2] = t1
+      eval_list %{:vals => eval_operator(h2, map.vals, num), :list => t2}
+    end
+  end
+
+  defp eval_operator(op, vals, num) do
+    toadd = vals.toadd
+    totimes = vals.totimes
+    case op do
+      "+" -> %{:toadd => totimes * num + toadd, :totimes => 1}
+      "-" -> %{:toadd => totimes * num + toadd, :totimes => -1}
+      "*" -> %{:toadd => toadd, :totimes => totimes * num}
+      "/" -> %{:toadd => toadd, :totimes => totimes / num}
+    end
+  end
+
+  defp eval_list_by_close_paren(list) do
+    
+  end
+
+  def is_open_paren(head) do
+    head =~ ~r/^\([1-9][0-9]*$/ || head =~ ~r/^\(0$/
   end
 end
