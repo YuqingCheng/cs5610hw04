a4cd8bfbf0f186c315a5e1e7a6c9cc1c7bb0baed
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-02-03 00:06:58 -0500

    some progress in implementing stack

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -2,25 +2,39 @@ defmodule Calc do
   def eval(line) do
     list = String.split(line, " ")
     vals = %{:toadd => 0.0, :totimes => 1.0, :func => fn x, y -> x * y end}
-    map = %{:vals => vals, :list => list}
+    map = %{:vals => vals, :list => list :stack => []}
     eval_list(map)
   end
 
   defp eval_list(map) do
     [h1 | t1] = map.list
+    plus = fn x, y => x + y
+    func = map.vals.func
+    toadd = map.vals.toadd
+    totimes = map.vals.totimes
+    new_vals = %{:toadd => 0.0, :totimes => 1.0, :func => fn x, y -> x * y end}
 
-    if is_open_paren h1 do
-      temp = String.slice(h1, 1, String.length(h1))
-    end
+    cond do
+      is_open_paren h1 && is_close_paren h1 ->
+        temp = String.slice(h1, 1, String.length(h1)-1)
+        eval_list %{map | :list => [temp] ++ t1}
+      is_open_paren h1 ->
+        temp = String.slice(h1, 1, String.length(h1))
+        eval_list %{:vals => new_vals, :list => [temp] ++ t1, :stack => [map.vals] ++ map.stack} 
+      is_close_paren h1 ->
+        temp = String.slice(h1, 0, String.length(h1)-1)
+        
 
-    num = Float.parse(h1) |> elem(0)
+      true ->
+        num = Float.parse(h1) |> elem(0)
 
-    if length(t1) == 0 do
-      map.vals.func.(map.vals.totimes, num) + map.vals.toadd
-    else
-      [h2 | t2] = t1
-      eval_list %{:vals => eval_operator(h2, map.vals, num), :list => t2}
-    end
+        if length(t1) == 0 do
+          func.(totimes, num) + toadd
+        else
+          [h2 | t2] = t1
+          eval_list %{:vals => eval_operator(h2, map.vals, num), :list => t2, :paren => map.paren}
+        end
+    end    
   end
 
   defp eval_operator(op, vals, num) do
