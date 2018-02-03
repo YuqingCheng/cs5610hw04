c3c8476eef8a055e1b09f9d74549fc9bb0f7cb6c
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-02-03 10:32:16 -0500

    calculator works fine

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -17,22 +17,33 @@ defmodule Calc do
       is_open_paren(h1) && is_close_paren(h1) ->
         temp = String.slice(h1, 1, String.length(h1)-1)
         eval_list %{map | :list => [temp] ++ t1}
+
       is_open_paren h1 ->
         temp = String.slice(h1, 1, String.length(h1))
         eval_list %{:vals => new_vals, :list => [temp] ++ t1, :stack => [map.vals] ++ map.stack} 
+        
       is_close_paren h1 ->
-        temp = String.slice(h1, 0, String.length(h1)-1)
-        [pop | stack] = map.stack
-        tuple = Float.parse(temp)
-        num = elem(tuple, 0)
-        remain = elem(tuple, 1)
+        if length(map.stack) == 0 do
+          wrong_input
+        else
+          temp = String.slice(h1, 0, String.length(h1)-1)
+          [pop | stack] = map.stack
+          tuple = Float.parse(temp)
+          num = elem(tuple, 0)
+          remain = elem(tuple, 1)
 
-        head = pop.func.(pop.totimes, func.(totimes, num) + toadd) + pop.toadd
-        eval_list %{:vals => new_vals, :list => [Float.to_string(head) <> remain] ++ t1, :stack => stack}
+          head = pop.func.(pop.totimes, func.(totimes, num) + toadd) + pop.toadd
+          eval_list %{:vals => new_vals, :list => [Float.to_string(head) <> remain] ++ t1, :stack => stack}
+        end
+        
       true ->
         num = parse(h1)
         if length(t1) == 0 do
-          func.(totimes, num) + toadd
+          if length(map.stack) > 0 do
+            wrong_input
+          else
+            func.(totimes, num) + toadd
+          end
         else
           [h2 | t2] = t1
           eval_list %{:vals => eval_operator(h2, map.vals, num), :list => t2, :stack => map.stack}
@@ -74,5 +85,9 @@ defmodule Calc do
     else
       Float.parse(num) |> elem(0)
     end
+  end
+
+  def wrong_input do
+    "wrong input"
   end
 end
