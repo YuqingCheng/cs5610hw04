a909882496e7bb71044ef1aef2a700d1c1384b01
Yuqing Cheng <cheng.yuq@husky.neu.edu>
2018-02-02 22:57:32 -0500

    fix bug for divide

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -1,10 +1,9 @@
 defmodule Calc do
   def eval(line) do
     list = String.split(line, " ")
-    vals = %{:toadd => 0.0, :totimes => 1.0}
+    vals = %{:toadd => 0.0, :totimes => 1.0, :func => fn x, y -> x * y end}
     map = %{:vals => vals, :list => list}
     eval_list(map)
-
   end
 
   defp eval_list(map) do
@@ -17,7 +16,7 @@ defmodule Calc do
     num = Float.parse(h1) |> elem(0)
 
     if length(t1) == 0 do
-      map.vals.totimes * num + map.vals.toadd
+      map.vals.func.(map.vals.totimes, num) + map.vals.toadd
     else
       [h2 | t2] = t1
       eval_list %{:vals => eval_operator(h2, map.vals, num), :list => t2}
@@ -27,11 +26,12 @@ defmodule Calc do
   defp eval_operator(op, vals, num) do
     toadd = vals.toadd
     totimes = vals.totimes
+    func = vals.func
     case op do
-      "+" -> %{:toadd => totimes * num + toadd, :totimes => 1}
-      "-" -> %{:toadd => totimes * num + toadd, :totimes => -1}
-      "*" -> %{:toadd => toadd, :totimes => totimes * num}
-      "/" -> %{:toadd => toadd, :totimes => totimes / num}
+      "+" -> %{:toadd => func.(totimes, num) + toadd, :totimes => 1, :func => fn x, y -> x*y end}
+      "-" -> %{:toadd => func.(totimes, num) + toadd, :totimes => -1, :func => fn x, y -> x*y end}
+      "*" -> %{:toadd => toadd, :totimes => func.(totimes, num), :func => fn x, y -> x*y end}
+      "/" -> %{:toadd => toadd, :totimes => func.(totimes, num), :func => fn x, y -> x/y end}
     end
   end
 
@@ -40,6 +40,6 @@ defmodule Calc do
   end
 
   def is_open_paren(head) do
-    head =~ ~r/^\([1-9][0-9]*$/ || head =~ ~r/^\(0$/
+    head =~ ~r/^\(/
   end
 end
